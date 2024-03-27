58f1c9179bd074ddcb3cdc7363cc5df44ef44689
Hennse4n <165119064+Hennse4n@users.noreply.github.com>
2024-03-27 14:17:49 -0600

    form.php

--- php/form.php
+++ php/form.php
@@ -1,22 +1,21 @@
 <?php
-$person_name = $_POST['person_name'];
-$person_Lname = $_POST['person_Lname'];
-$person_mail = $_POST['person_mail'];
-$msg = $_POST['msg'];
+$nombre = $_POST['person_name'];
+$apellido = $_POST['person_Lname'];
+$correo = $_POST['person_mail'];
+$asunto = $_POST['msg'];
 
-$header = 'From: '. $person_mail. "\r\n";
-$header = "X-Mailer: PHP/". phpversion(). "\r\n";
-$header = "Mime-Version: 1.0 \r\n";
-$header = "Content-Type: text/plain";
+$D_form = "
+	Nombre: $nombre\n
+	Apellido: $apellido\n
+	Correo: $correo\n
+"
 
-$msg = "Este mensaje fue enviado por: ". $person_name. $person_Lname. "\r\n";
-$msg = "Con el correo: ". $person_mail. "\r\n";
-$msg = "Con el asunto de: ". $_POST['msg']. "\r\n";
-$msg = "Con fecha de: ". date('d/m/y', time()). "\r\n";
+$mail = "hennsen1287@gmail.com";
 
-$para = 'hennsen1287@gmail.com';
-$asunto = 'Asunto del Mensaje';
+$mensaje = $asunto;
 
-mail($para, $asunto, utf8_decode($msg)$header);
-header("Location: Gracias.html);
+$header = "From: $email\r\n";
+$header = "Content-Type: text/plain: charset=UTF-8";
+$email($mail, $mensaje, $D_form, $header) or die("Error!");
+$header("Location: Gracias.html");
 ?>
