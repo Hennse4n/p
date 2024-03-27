2c5c7df758a04cedc49c8c40e4785a484aaf8334
Hennse4n <165119064+Hennse4n@users.noreply.github.com>
2024-03-27 15:49:58 -0600

    form.php

--- php/form.php
+++ php/form.php
@@ -1,21 +1,22 @@
 <?php
-$nombre = $_POST['person_name'];
-$apellido = $_POST['person_Lname'];
-$correo = $_POST['person_mail'];
-$asunto = $_POST['msg'];
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $nombre = $_POST["nombre"];
+    $email = $_POST["email"];
+    $mensaje = $_POST["mensaje"];
 
-$d_form = "
-	Nombre: $nombre\n
-	Apellido: $apellido\n
-	Correo: $correo\n
-"
+    $destinatario = "hennsen1287@gmail.com"; // Cambia esto al correo electrónico al que quieras enviar el mensaje
+    $asunto = "Mensaje enviado desde el formulario de contacto";
 
-$mail = "hennsen1287@gmail.com";
+    $contenido = "Nombre: $nombre\n";
+    $contenido .= "Email: $email\n\n";
+    $contenido .= "Mensaje:\n$mensaje";
 
-$mensaje = $asunto;
-
-$header = "From: $email\r\n";
-$header = "Content-Type: text/plain: charset=UTF-8";
-$email($mail, $mensaje, $d_form, $header) or die("Error!");
-$header("Location: Gracias.html");
+    // Envía el correo electrónico
+    if (mail($destinatario, $asunto, $contenido)) {
+        echo "El mensaje ha sido enviado correctamente.";
+    } else {
+        echo "Hubo un error al enviar el mensaje.";
+    }
+}
 ?>
+
