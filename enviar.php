aab104bce1ac2bb8912fd089da4da7aee9c297e2
Hennse4n <165119064+Hennse4n@users.noreply.github.com>
2024-03-27 16:00:53 -0600

    Create enviar.php

---
+++ enviar.php
@@ -0,0 +1,21 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $nombre = $_POST["nombre"];
+    $email = $_POST["email"];
+    $mensaje = $_POST["mensaje"];
+
+    $destinatario = "destinatario@example.com"; // Cambia esto al correo electrónico al que quieras enviar el mensaje
+    $asunto = "Mensaje enviado desde el formulario de contacto";
+
+    $contenido = "Nombre: $nombre\n";
+    $contenido .= "Email: $email\n\n";
+    $contenido .= "Mensaje:\n$mensaje";
+
+    // Envía el correo electrónico
+    if (mail($destinatario, $asunto, $contenido)) {
+        echo "El mensaje ha sido enviado correctamente.";
+    } else {
+        echo "Hubo un error al enviar el mensaje.";
+    }
+}
+?>
