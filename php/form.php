3cdea849e8ae236efc22b64839a6bb417ed65cc5
Hennse4n <165119064+Hennse4n@users.noreply.github.com>
2024-03-27 15:03:43 -0600

    form.php

--- php/form.php
+++ php/form.php
@@ -4,7 +4,7 @@ $apellido = $_POST['person_Lname'];
 $correo = $_POST['person_mail'];
 $asunto = $_POST['msg'];
 
-$D_form = "
+$d_form = "
 	Nombre: $nombre\n
 	Apellido: $apellido\n
 	Correo: $correo\n
@@ -16,6 +16,6 @@ $mensaje = $asunto;
 
 $header = "From: $email\r\n";
 $header = "Content-Type: text/plain: charset=UTF-8";
-$email($mail, $mensaje, $D_form, $header) or die("Error!");
+$email($mail, $mensaje, $d_form, $header) or die("Error!");
 $header("Location: Gracias.html");
 ?>
